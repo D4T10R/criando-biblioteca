ce46fb079b93120daed096af600cbbd8c818b044
Datior <leonardowederveiga@yahoo.com>
2023-05-02 13:34:23 -0300

    Criando um projeto em node.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+// const chalk = require('chalk')
+
+console.log(chalk.blue('olá mundo'));
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento ');
+console.log('olá mundo');
