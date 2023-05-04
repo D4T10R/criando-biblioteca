6b04ba0f887d89051b72f61f1290abef100a1964
Datior <leonardowederveiga@yahoo.com>
2023-05-03 23:07:28 -0300

    Capturando links

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import chalk from "chalk";
 import fs from "fs";
 // const chalk = require('chalk')
 
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captura = [...texto.matchAll(regex)];
+    const resultados = captura.map(capt => ({[capt[1]]: capt[2]}))
+    return resultados
+}
+
 function trataErro(erro) {
     console.log(erro)
     throw new Error(chalk.red(erro.code, 'Não encontramos o caminho do arquivo'));
@@ -23,7 +31,7 @@ async function pegaArquivo(caminhoArquivo) {
         const encoding = 'utf-8'
         const texto = await fs.promises.readFile(caminhoArquivo, encoding)
     
-        console.log(chalk.green(texto))    
+        console.log(extraiLinks(texto))
     } catch(err) {
         trataErro(err)
     }
